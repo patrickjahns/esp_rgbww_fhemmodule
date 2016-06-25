9cba42709a763902f158e2cb5ac4afe9447bbbc4
Constantin Krüger <Constantinidis@web.de>
2016-06-25 08:25:47 +0200

    add script to generate controls_ledcontroller.txt for update all ...

---
+++ prepare_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash   
+rm controls_ledcontroller.txt
+find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
+  do
+   echo "DEL ${f}" >> controls_ledcontroller.txt
+   out="UPD "$(stat -c %y  $f | cut -d. -f1 | awk '{printf "%s_%s",$1,$2}')" "$(stat -c %s $f)" ${f}"
+   echo ${out//.\//} >> controls_ledcontroller.txt
+done
+
+# CHANGED file
+echo "LedController last change:" > CHANGED
+echo $(date +"%Y-%m-%d") >> CHANGED
+echo " - $(git log -1 --pretty=%B)" >> CHANGED
+
+
+
+
+
+
